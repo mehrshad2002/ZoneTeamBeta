23e34b86a38f84d136d394ef47b3a577ba7532c4
Mehrshad <amehrsahd54@gmail.com>
2022-04-08 15:37:41 +0430

    Start Pages

--- zonebeta/hub/urls.py
+++ zonebeta/hub/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('' , views.HubIndex , name = "hub")
+    path('' , views.HubIndex , name = "hub"),
+    path('create/' , views.CreateIndex , name = "create") 
 ]
--- zonebeta/hub/views.py
+++ zonebeta/hub/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .forms import HubForms
 
 def HubIndex(request):
     return render(request, 'hub/hub.html')
+
+
+def CreateIndex(request):
+    return render(request, 'hub/create.html')
